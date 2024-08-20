7043cc89c0ec6484d6fae809d63f99dbe8a98dca
A'DI <EquereAedidiong@gmail.com>
2024-08-20 11:17:44 +0100

    Fetching Data from an API and Displaying It

---
+++ fetch-data.js
@@ -0,0 +1,41 @@
+// Initialize the Async Function
+
+async function fetchUserData () {
+
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users'
+
+    dataContainer = document.getElementById('api-data')
+   
+    try {
+
+      const response = await fetch (apiUrl)
+      const users = await response.json
+      dataContainer.innerHTML = ''
+      const userList = document.createElement('ul');
+      users.array.forEach(users => {
+            const listItem = document.createElement('li')
+            listItem.textContent = user.name;
+            userList.appendChild(listItem);
+        });
+
+        dataContainer.appendChild(userList);
+    
+    }
+
+    //Error Handling
+
+
+    catch (error) {
+
+        dataContainer.innerHTML = ''
+        dataContainer.innerHTML.textContent = 'Failed to load user data'
+       console.error (error)
+    }
+
+}
+
+document.addEventListener ('DOMContentLoaded', function () {
+
+    fetchUserData ();
+})
+
